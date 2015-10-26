75b70e9ed0ad8d89c7d6a1d88deaa7559666ee33
Hans Chan <chenhc@ucweb.com>
2015-10-26 13:47:40 +0800

    支持 fis 的 __uri() 资源定位

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var path = require('path');
+var through = require('through');
 var deasync = require('deasync');
 var browserify = require('browserify');
 var stringify = require('stringify');
@@ -12,6 +14,7 @@ module.exports = function (content, file, conf) {
         browserify(file.realpath, conf.opts || {})
         .transform(stringify(['.tpl', '.html'])) // 支持 require(tpl/html)
         .transform(debowerify) // 支持 bower
+        .transform(urify) // 支持 fis 的 __uri() 资源定位
         .on('file', function (depFilePath) {
             // find dependences
             if (depFilePath !== file.realpath) {
@@ -34,3 +37,38 @@ module.exports = function (content, file, conf) {
     }
     return content;
 };
+
+
+var fisCompileSettings = (fis.compile || '').settings || {};
+var URI_REG = /\b__uri\(\s*('|")([^'"]+)\1\s*\)/g;
+/**
+ * Browserify transform
+ * change `__uri('xxx')` to real path
+ * @param  {string} file [description]
+ */
+function urify(inputFileRealPath) {
+    var chunks = [];
+
+    var onwrite = function (buffer) {
+      chunks.push(buffer);
+    };
+
+    var onend = function () {
+        var contents = Buffer.concat(chunks)
+            .toString('utf8')
+            .replace(URI_REG, function (match, quotmark, depFileName) {
+                var depFile = fis.uri(depFileName, path.dirname(inputFileRealPath));
+                if (depFile && depFile.file) {
+                    var url = depFile.file.getUrl(fisCompileSettings.hash, fisCompileSettings.domain);
+                    return quotmark + url + quotmark;
+                } else {
+                    console.error('\n' + depFileName + ' NOT found from ' + inputFileRealPath);
+                    return match;
+                }
+            });
+      this.queue(contents);
+      this.queue(null);
+    };
+
+    return through(onwrite, onend);
+}
